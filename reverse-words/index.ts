1822973a4396c0977cea28c4ffcf9a75d09cccc1
somleff <somleff@gmail.com>
2023-05-26 14:45:12 +0200

    reverseWords Refactor

--- reverse-words/index.ts
+++ reverse-words/index.ts
@@ -1,22 +1,27 @@
-function reverseWords(line: string) {
-    const wordArray = line.split(' ');
+function reverseMyWord(word: string[]):string {
+    let reversedChars = [];
+
+    for (let j = 0; j < word.length; j++) {
+        reversedChars.unshift(word[j]);
+    }
+
+    return reversedChars.join('');
+}
+
+function reverseWords(text: string):string {
+    const arrayFromText = text.split(' ');
     let pushInMe = [];
 
-    for (let i = 0; i < wordArray.length; i++) {
-        const word = wordArray[i].split('');
-        let reversedChars = [];
+    for (let i = 0; i < arrayFromText.length; i++) {
+        let word = arrayFromText[i].split('');
+        let reversedWord = reverseMyWord(word);
 
-
-        for (let j = 0; j < word.length; j++) {
-            reversedChars.unshift(word[j]);
-        }
-        pushInMe.push(reversedChars.join(''));
+        pushInMe.push(reversedWord);
     }
     return pushInMe.join(' ');
 }
 
-console.log(reverseWords('this is a string of words'));
-// siht si a gnirts fo sdrow
+console.log(reverseWords('this is a string of words')); // siht si a gnirts fo sdrow
 
 export default reverseWords;
 
