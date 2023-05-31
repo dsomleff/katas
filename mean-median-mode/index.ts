63d4bf88ac4749cb1c542e00a147f371419be225
somleff <somleff@gmail.com>
2023-05-31 14:00:45 +0200

    Mean Median Mode: getMean function

--- mean-median-mode/index.ts
+++ mean-median-mode/index.ts
@@ -1,13 +1,31 @@
-function getMean(arr: number[]) {} // logic to calculate Mean
-function getMedian(arr: number[]) {} // logic to calculate Median
-function getMode(arr: number[]){} // logic to calculate Mode
-function meanMedianMode(arr: number[]): void {
-    getMean(arr);
-    getMedian(arr);
-    getMode(arr);
+// logic to calculate Mean
+function getMean(arr: number[]) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum / arr.length;
 }
 
-console.log(meanMedianMode([9, 10, 23, 10, 23, 9]));
+// logic to calculate Median
+function getMedian(arr: number[]) {}
+
+// logic to calculate Mode
+function getMode(arr: number[]){}
+
+function meanMedianMode(arr: number[]): object {
+    const mean = getMean(arr);
+    getMedian(arr);
+    getMode(arr);
+
+    return {
+        mean
+    }
+}
+
+console.log(meanMedianMode([1,2,3,4,5,4,6,1]));
 
 // meanMedianMode([1,2,3,4,5,4,6,1]);
 // {
