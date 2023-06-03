91c5e5deda222c5778bbc7d8519592071c9f2ae8
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-06-03 12:58:52 +0200

    2 Sum: recursion draft

--- 2sum/index.ts
+++ 2sum/index.ts
@@ -1,7 +1,7 @@
 /**
  * O(n^{2}) solution
  */
-function twoSum(numArr: number[], num: number): [number, number][] {
+function twoSumOn2(numArr: number[], num: number): [number, number][] {
     let element: number;
     let res = new Map();
 
@@ -14,12 +14,31 @@ function twoSum(numArr: number[], num: number): [number, number][] {
         }
     }
     return Array.from(res);
-
 }
 
-// const numArr = [1, 6, 4, 5, 3, 3];
-const numArr = [40, 11, 19, 17, -12];
-// console.log(twoSum(numArr, 7));; // [[6,1], [3, 4], [3, 4]]
-console.log(twoSum(numArr, 28)); // [[17, 11], [-12, 40]]
+/**
+ * Recursion solution
+ */
+function twoSumRecursion(numArr: number[], num: number, res: [number, number][] | undefined = []) {
+    if (res === undefined) res = [];
+
+    if (numArr.length <= 0) return res;
+    const firstElement = numArr.shift();
+
+    for (let i = 0; i < numArr.length; i++) {
+        if (firstElement && (firstElement + numArr[i] === num)) {
+            res.push([firstElement, numArr[i]]);
+        }
+    }
+
+    twoSumRecursion(numArr, num, res);
+
+    return res;
+}
+
+const numArr = [1, 6, 4, 5, 3, 3];
+console.log(twoSumOn2(numArr, 7)); // [[6,1], [3, 4], [3, 4]]
+const numArr2 = [40, 11, 19, 17, -12];
+console.log(twoSumRecursion(numArr2, 28)); // [[17, 11], [-12, 40]]
 
 
