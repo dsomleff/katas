9ab6ed720eafa518dc72e69cb9aac1bfdfc30736
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-06-05 18:58:36 +0200

    Dynamic Array: insert + tests

--- dynamic-array/index.test.ts
+++ dynamic-array/index.test.ts
@@ -1,14 +1,27 @@
 import { DynamicArray } from './index';
 
-describe('Dynamic Array', function () {
+describe('Dynamic da', function () {
     let da: DynamicArray;
 
     beforeEach(() => {
-        da = new DynamicArray(2);
+        da = new DynamicArray(4);
     });
 
     it('should get and set values', () => {
         da.set(0, "a");
         expect(da.get(0)).toBe('a');
+    });
+
+    it('should insert value into specific index', () => {
+        da.set(0, "a");
+        da.set(1, "b");
+        da.set(2, "c");
+        da.insert(1, "d");
+
+        expect(da.size).toBe(4);
+        expect(da.get(0)).toBe('a');
+        expect(da.get(1)).toBe('d');
+        expect(da.get(2)).toBe('b');
+        expect(da.get(3)).toBe('c');
     });
 });
--- dynamic-array/index.ts
+++ dynamic-array/index.ts
@@ -3,7 +3,8 @@ interface DA {
     size: number,
     initialCapacity: number,
     set(index: number, value: string): string,
-    get(index: number): string
+    get(index: number): string,
+    insert(index: number, value: string): any,
 }
 export class DynamicArray implements DA{
     public readonly data: string[];
@@ -16,13 +17,27 @@ export class DynamicArray implements DA{
     }
 
     set(index: number, value: string): string {
+        this.size++;
         return this.data[index] = value;
     }
 
     get(index: number): string {
         return this.data[index];
     }
+
+    insert(index: number, value: string): any {
+        if (!this.data[index]) return this.set(index, value);
+
+        for (let i = this.data.length - 1; i >= index; i--) {
+            this.data[i + 1] = this.data[i];
+        }
+        this.set(index, value);
+    }
 }
 
 const da = new DynamicArray(2);
+da.set(0, 'a');
+da.set(1, 'c');
+da.set(2, 'd');
+da.insert(1, 'b');
 console.log(da);
