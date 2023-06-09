cfdc2f25ae83891e877e4db614681c734a9e3a56
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-06-09 14:23:46 +0200

    Two Crystal Ball: solution

---
+++ two_crystal_ball/index.ts
@@ -0,0 +1,24 @@
+function ball_problem(floors: boolean[]): number {
+   const jump = Math.floor(Math.sqrt(floors.length));
+   let i = jump;
+
+   for (; i < floors.length; i+=jump) {
+       if (floors[i]) {
+           break;
+       }
+   }
+
+   i -= jump;
+
+   for (let j = 0; j <= jump && i < floors.length; j++, i++) {
+       if (floors[i]) {
+           return i;
+       }
+   }
+
+   return -1;
+}
+
+const floors = [false, false, false, false, false, false, false, true, true, true];
+console.log(ball_problem(floors));
+
