a6145ac4c71a91d113866e7659dd3431acd021e8
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-06-10 10:27:59 +0200

    Fibonacci: Solution

---
+++ fibonacci/index.ts
@@ -0,0 +1,8 @@
+function fibonacci(position: number): any {
+    if (position < 2) return position;
+
+    return fibonacci(position - 1) + fibonacci(position - 2);
+}
+
+console.log(fibonacci(4)); // 3
+// Fibonacci Sequence: 1,1,2,3,5,8,13,21,34
