73cade4db0fb614764acc9b0a685f254da361153
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-06-26 14:13:10 +0200

    Running Sum
    - Polishing

--- running-sum/index.ts
+++ running-sum/index.ts
@@ -2,10 +2,12 @@ export function runningSum(nums: number[]): number[] {
   let res = [];
 
   res.push(nums[0]);
+
   for (let i = 1; i < nums.length; i++) {
     let calculated: number = nums[i] + res[res.length - 1];
     res.push(calculated);
   }
+
   return res;
 }
 const nums = [1, 2, 3, 4];
