97343db9a934d00df52644ccf11351652d63a19e
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-07-01 16:17:48 +0200

    Sort People
    - Solution + unit tests

--- sort_the_people/index.test.ts
+++ sort_the_people/index.test.ts
@@ -1,3 +1,45 @@
 import { sortPeople } from "./index";
 
-describe("Sort People", () => {});
+describe("Sort People", () => {
+  it("should sort people by heights in descending order", () => {
+    const names = ["John", "Jane", "Doe"];
+    const heights = [170, 180, 160];
+    const expected = ["Jane", "John", "Doe"];
+    const result = sortPeople(names, heights);
+    expect(result).toEqual(expected);
+  });
+
+  it("should return an empty array if both names and heights are empty", () => {
+    const names: [] = [];
+    const heights: [] = [];
+    const expected: [] = [];
+    const result = sortPeople(names, heights);
+    expect(result).toEqual(expected);
+  });
+
+  it("should handle arrays with a single element", () => {
+    const names = ["John"];
+    const heights = [170];
+    const expected = ["John"];
+    const result = sortPeople(names, heights);
+    expect(result).toEqual(expected);
+  });
+
+  it("should handle arrays with elements of the same height", () => {
+    const names = ["John", "Jane", "Doe"];
+    const heights = [170, 170, 170];
+    const expected = ["John", "Jane", "Doe"];
+    const result = sortPeople(names, heights);
+    expect(result).toEqual(expected);
+  });
+
+  it("should sort people with the same name but different heights", () => {
+    const names = ["John", "John", "John"];
+    const heights = [170, 180, 160];
+    const expected = ["John", "John", "John"];
+    const result = sortPeople(names, heights);
+    const expectedHeights = [180, 170, 160];
+    expect(result).toEqual(expected);
+    expect(heights).toEqual(expectedHeights);
+  });
+});
--- sort_the_people/index.ts
+++ sort_the_people/index.ts
@@ -1,1 +1,24 @@
-function sortPeople(names: string[], heights: number[]): string[] {}
+export function sortPeople(names: string[], heights: number[]): string[] {
+  let unsortedUntilIndex = heights.length - 1;
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+
+    for (let i = 0; i < unsortedUntilIndex; i++) {
+      if (heights[i] < heights[i + 1]) {
+        sorted = false;
+        // Swap elements
+        [heights[i], heights[i + 1]] = [heights[i + 1], heights[i]];
+        [names[i], names[i + 1]] = [names[i + 1], names[i]];
+      }
+    }
+    unsortedUntilIndex -= 1;
+  }
+  console.log({ names });
+  return names;
+}
+
+let names = ["Alice", "Bob", "Bob"],
+  heights = [155, 185, 150];
+sortPeople(names, heights);
